126a2ee897c01cff94e74444118e91d3ce80b30b
michaelschmelz <michael.schmelz86@gmail.com>
2022-03-14 19:04:39 -0500

    test

---
+++ test.ps1
@@ -0,0 +1,16 @@
+function Test-MrVerboseOutput {
+
+    [CmdletBinding()]
+    param (
+        [ValidateNotNullOrEmpty()]
+        [string[]]$ComputerName = $env:COMPUTERNAME
+    )
+
+    foreach ($Computer in $ComputerName) {
+        Write-Verbose -Message "Attempting to perform some action on $Computer"
+        Write-Output $Computer
+    }
+
+}
+
+Test-MrVerboseOutput -Verbose
